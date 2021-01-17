1a394d41ffee0a6b9745c1f882ff6b52053cef39
7g0r <40136311+7g0r@users.noreply.github.com>
2021-01-17 21:56:08 +0100

    Initial

---
+++ src/test/java/com/chmielewski/clinic_app/ClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chmielewski.clinic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
