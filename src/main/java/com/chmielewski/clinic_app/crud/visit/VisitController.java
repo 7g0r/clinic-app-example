7d834286291a3e7789af700fd084fbb55f4aed5f
7g0r <40136311+7g0r@users.noreply.github.com>
2021-01-18 20:06:00 +0100

    Controller to find Visits By Day and DoctorId Works

--- src/main/java/com/chmielewski/clinic_app/crud/visit/VisitController.java
+++ src/main/java/com/chmielewski/clinic_app/crud/visit/VisitController.java
@@ -2,10 +2,11 @@ package com.chmielewski.clinic_app.crud.visit;
 
 import com.chmielewski.clinic_app.crud.abstracts.AbstractController;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/visits")
@@ -20,8 +21,18 @@ public class VisitController extends AbstractController<VisitService, VisitDto> 
 
 
     @GetMapping(path = "/user/{id}")
-    public Page<VisitDto> getVisitsByUserId(@PathVariable("id") Long userId){
+    public Page<VisitDto> getVisitsByUserId(@PathVariable("id") Long userId) {
         return visitService.findVisitsByUserId(userId);
+    }
+
+    @GetMapping(path = "/doctor/{id}")
+    public Page<VisitDto> getVisitsByDoctorId(@PathVariable("id") Long doctorId) {
+        return visitService.findVisitsByDoctorId(doctorId);
+    }
+
+    @GetMapping(path = "/date/{id}")
+    public Page<VisitDto> getVisitsByDateAndDoctorId(@PathVariable("id") Long doctorId, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        return visitService.findVisitsByDoctorIdAndDate(doctorId, date);
     }
 
 }
--- src/main/java/com/chmielewski/clinic_app/crud/visit/VisitRepository.java
+++ src/main/java/com/chmielewski/clinic_app/crud/visit/VisitRepository.java
@@ -3,6 +3,8 @@ package com.chmielewski.clinic_app.crud.visit;
 import com.chmielewski.clinic_app.crud.abstracts.CommonRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -11,4 +13,6 @@ public interface VisitRepository extends CommonRepository<Visit> {
     List<Visit> findVisitByUserId(Long userId);
 
     List<Visit> findAllByDoctorId(Long doctorId);
+
+    List<Visit> findAllByDoctorIdAndVisitStarts(Long doctorId, LocalDate date);
 }
